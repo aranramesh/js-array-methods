cd04ff28f07daa664277be686f9d3adbe84ca21b
aranramesh <svramesh692@gmail.com>
2022-10-13 16:32:35 +0530

    port

---
+++ app.js
@@ -0,0 +1,6 @@
+import {a} from "./main.js";
+console.log(a);
+
+import {user, res} from "./main.js";
+console.log(user ,res);
+
---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+// this keyword
+//     var obj ={
+//     name:'react',
+//     getname:function(){
+// console.log(this.name)
+//     },
+//     getarrowname: () => {
+//         console.log(this.name)
+//     }
+// }
+
+export const a = [1,2,3,4,5,6];
+export let user ="Ramesh";
+
+ export let res = user.split("").reverse().join("");
+ 
